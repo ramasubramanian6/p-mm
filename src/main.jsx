a03006f11310e41f2a8e5787b7f8c1ec58d41ccf
ramasubramanian6 <ramsubramanianponni37@gmail.com>
2025-06-09 16:52:18 +0530

    change the broswer route

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "../src/pages/redux/store";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
